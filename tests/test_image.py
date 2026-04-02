148dd143eb09877bbf9720dbfc5660afa7e8dbba
Ada Bohm <ada@kreatrix.org>
2026-04-02 16:15:16 +0200

    WIP

---
+++ tests/test_image.py
@@ -0,0 +1,35 @@
+from alsie import *
+
+
+def test_svg_image(test_scene):
+    with test_scene:
+        image("assets/test.svg").size(50, 40)
+
+
+def test_svg_image_width_dtd(test_scene):
+    with test_scene:
+        image("assets/knight_with_dtd.svg").size(50, 40)
+
+
+def test_png_image(test_scene):
+    with test_scene:
+        image("assets/testimg.png").size(50, 40)
+
+
+def test_jpeg_image(test_scene):
+    with test_scene:
+        image("assets/testimg.jpeg").size(50, 40)
+
+
+def test_ora_image(test_scene):
+    with test_scene:
+        image("assets/test.ora").size(50, 40)
+
+
+def test_svg_image_layer_remove(test_scene):
+    with test_scene:
+        image("assets/test.svg").size(50, 40).layer("Ball").remove()
+
+def test_ora_image_layer_remove(test_scene):
+    with test_scene:
+        image("assets/test.ora").size(50, 40).layer("B").remove()
